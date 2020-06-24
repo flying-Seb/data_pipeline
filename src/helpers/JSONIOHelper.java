9df71e6be314ddacec65b2520986ddd4ac3eee4e
Sebastian Vogeltanz <sebvogeltanz@gmail.com>
2020-06-24 12:27:42 +0200

    added new methods to lemmatise the text and store it back to JSON

--- src/helpers/JSONIOHelper.java
+++ src/helpers/JSONIOHelper.java
@@ -9,87 +9,127 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 public class JSONIOHelper {
-	
+
 	// create class variables for two JSON Objects
 	JSONObject rootObject;
 	JSONObject documentsObject;
-	
+	JSONObject lemmasObject;
+
 	// create three methods to structure, update and save JSON data
 	public void CreateBasicJSONStructure() {
 		// create new instances of the Objects (Step 1)
 		rootObject = new JSONObject();
 		documentsObject = new JSONObject();
-		
+		// create new object (lemmasObject) and add it to the rootObject (2nd)
+		lemmasObject = new JSONObject();
+
+		// add both "child" objects to the root object
 		rootObject.put("documents", documentsObject);
+		rootObject.put("lemmas", lemmasObject);
+
 	}
-	
+
 	public void AddDocumentsToJSONStructure(ConcurrentHashMap<String, String> documents) {
-		//adding file data (from B1TextLoader) to JSON data structure (Step 2)
-		
-		// creating a for each loop to iterate over 
+		// adding file data (from B1TextLoader) to JSON data structure (Step 2)
+
+		// creating a for-each loop to iterate over the documents concurrent hash map
 		for (Entry<String, String> entry : documents.entrySet()) {
 			documentsObject.put(entry.getKey(), entry.getValue());
 		}
-		
+
 	}
 	
+	public void AddLemmasToJSONStructure(ConcurrentHashMap<String, String> lemmas) {
+		// a method to convert the lemmatised text into JSONStructure
+		for (Entry<String, String> entry : lemmas.entrySet()) {
+			lemmasObject.put(entry.getKey(), entry.getValue());
+		}
+	}
+
 	public void SaveJSON(String filename) {
 		// saving the data into a .json file
-		
+
 		// creating a String out of the rootObject
 		String JSONString = rootObject.toJSONString();
-		
+
 		// creating a new instance of the FileWriter class
-		try(FileWriter writer = new FileWriter(filename)){
+		try (FileWriter writer = new FileWriter(filename)) {
 			// writing the String to a file
 			writer.write(JSONString);
 			// print completion message to the user
 			System.out.println("Saving JSON to file successfully completed.");
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			System.out.println("Saving JSON to file failed...");
-		};
+		}
+		;
 	}
-	
+
 	public void LoadJSON(String filename) {
 		// A method to load the JSON data from the .json file
-		
+
 		// call the method to create the basic JSON structure
 		CreateBasicJSONStructure();
-		
+
 		// try-catch-block for opening a FileReader and load the data
 		// because of this design "file.close()" is not necessary
-		try(FileReader file = new FileReader(filename)){
+		try (FileReader file = new FileReader(filename)) {
 			// load the data
 			JSONParser parser = new JSONParser();
-			
-			rootObject = (JSONObject)parser.parse(file);
-			
-			if(rootObject.get("documents") != null) {
-				documentsObject = (JSONObject)rootObject.get("documents");
+
+			rootObject = (JSONObject) parser.parse(file);
+
+			if (rootObject.get("documents") != null) {
+				documentsObject = (JSONObject) rootObject.get("documents");
 			}
 
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
-		}
-		finally {
+		} finally {
 			System.out.println("JSON Loading complete. Lines: " + documentsObject.size());
 		}
 		
-		
-	}
-	
-	public ConcurrentHashMap<String,String> GetDocumentsFromJSONStructure(){
-		// create a concurrent hash map
-		ConcurrentHashMap<String,String> documents = new ConcurrentHashMap<String,String>();
-		
-		// iterate over the hash map and put every key into documents key
-		for(String key : (Iterable<String>)documentsObject.keySet()) {
-			documents.put(key, (String)documentsObject.get(key));
+		// another try-catch block for the lemmatised text
+		try (FileReader file = new FileReader(filename)){
+			JSONParser parser = new JSONParser();
+			
+			rootObject = (JSONObject) parser.parse(file);
+			
+			if(rootObject.get("lemmas") != null) {
+				lemmasObject = (JSONObject) rootObject.get("lemmas");
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			System.out.println("JSON Loading complete. Lines: " + lemmasObject.size());
 		}
+
+	}
+
+	public ConcurrentHashMap<String, String> GetDocumentsFromJSONStructure() {
+		// a method to get the documents from the JSON structure
 		
+		// create a concurrent hash map object
+		ConcurrentHashMap<String, String> documents = new ConcurrentHashMap<String, String>();
+
+		// iterate over the hash map and fill it with key of documents and text (casted to Strings)
+		for (String key : (Iterable<String>) documentsObject.keySet()) {
+			documents.put(key, (String) documentsObject.get(key));
+		}
+
 		return documents;
 	}
 	
+	public ConcurrentHashMap<String, String> GetLemmasFromJSONStructure(){
+		// a method to get the lemmatised text from the JSON structure
+		
+		// create a concurrent has map object
+		ConcurrentHashMap<String, String> lemmas = new ConcurrentHashMap<String, String>();
+		
+		// iterate over the hash map and fill it with key of lemmas and text (casted to Strings)
+		for (String key : (Iterable<String>) lemmasObject.keySet()) {
+			lemmas.put(key, (String) lemmasObject.get(key));
+		}
+		return lemmas;
+	}
+
 }
--- src/pipeline/B2Lemmatiser.java
+++ src/pipeline/B2Lemmatiser.java
@@ -1,5 +1,7 @@
 package pipeline;
 
+import java.util.List;
+import java.util.Map.Entry;
 import java.util.concurrent.ConcurrentHashMap;
 
 import edu.stanford.nlp.simple.*;
@@ -25,7 +27,41 @@ public class B2Lemmatiser {
 		
 		// call the concurrent hash map method to put the JSON into the outgoing data structure
 		ConcurrentHashMap<String,String> documents = JSONIO.GetDocumentsFromJSONStructure();
+		
+		ConcurrentHashMap<String,String> lemmatised = JSONIO.GetLemmasFromJSONStructure();
+		
+		// call the lemmatise method for every row of text in the documents object
+		for (Entry<String, String> entry : documents.entrySet()) {
+			lemmatised.put(entry.getKey(), LemmatiseSingleDoc(entry.getValue()));
+		}
+		
+		JSONIO.AddLemmasToJSONStructure(lemmatised);
+		JSONIO.SaveJSON("doc_and_lemm.json");
 
+	}
+	
+	private String LemmatiseSingleDoc(String text) {
+		// a method to lemmatise a single document which is passed as an argument
+		
+		// use regex to clean the data (remove most punctuation and replace several whitespace with a single one
+		text = text.replaceAll("\\p{Punct}", " ");
+		text = text.replaceAll("\\s+", " ");
+		
+		// remove all leading and trailing whitespace and set all to lowercase
+		text = text.replaceAll("^[ \\t]+|[ \\t]+$", "");
+		text = text.toLowerCase();
+		
+		// use the stanford nlp library to lemmatise the text
+		//first: create a sentence object and pass the text to it
+		Sentence sent = new Sentence(text);
+		// create a list of strings and call the method lemmas on it
+		List<String> lemmas = sent.lemmas();
+		// finally convert the List<String> back to a simple string and pass it to text with a whitespace as a delimiter
+		text = String.join(" ", lemmas);
+		
+		System.out.println(text);
+		
+		return text;
 	}
 
 }
