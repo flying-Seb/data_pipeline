73184e5bd3a31b2967a82e4a8c94cf0c2c68b46c
Sebastian Vogeltanz <sebvogeltanz@gmail.com>
2020-07-14 19:21:45 +0200

    solved the loading issue

--- src/pipeline/B3DescriptiveStatistics.java
+++ src/pipeline/B3DescriptiveStatistics.java
@@ -3,29 +3,30 @@ package pipeline;
 import java.util.Map.Entry;
 import java.util.concurrent.ConcurrentHashMap;
 import helpers.JSONIOHelper;
+import pipeline.B2Lemmatiser;
 
 
 
 public class B3DescriptiveStatistics {
 	
-	static JSONIOHelper loader = new JSONIOHelper();
+	static JSONIOHelper helper = new JSONIOHelper();
 	
 	public static void main(String[] args) {
 		
-		StartCreatingStatistics("JSONDataStore.json");
+		StartCreatingStatistics("ExampleOutput.txt");
 
 	}
 	
 	public static void StartCreatingStatistics(String filePath) {
 		System.out.println("Loading file...!");
 		
-		loader.LoadJSON(filePath);
+		// load the file with the helper class
+		helper.LoadJSON(filePath);
 		
-		ConcurrentHashMap<String, String> lemmas = loader.GetLemmasFromJSONStructure();;
+		// put it into a concurrent hash-map and fill it with the lemmas from the file
+		ConcurrentHashMap<String, String> lemmas = helper.GetLemmasFromJSONStructure();;
 		
-		// loader.LoadJSON(filePath);
-		// loader.GetLemmasFromJSONStructure();
-		
+		// print every entry to double-check the loading
 		for (Entry<String, String> entry : lemmas.entrySet()) {
 			System.out.println(entry.getKey() + ": " + entry.getValue());
 		}
